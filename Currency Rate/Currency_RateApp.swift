531dcbd94fad5426e728eb0e2832564d2ec4bfb2
Sergey Ivanov <ggdlin@gmail.com>
2021-05-25 00:06:37 +0300

    Initial Commit

---
+++ Currency Rate/Currency_RateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_RateApp.swift
+//  Currency Rate
+//
+//  Created by Ivanov Sergey on 25.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct Currency_RateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
